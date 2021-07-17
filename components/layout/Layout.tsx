fce2414e89ec753062df31463576b97f830e7af9
Niclas Overby <niclas@overby.me>
2021-07-17 10:31:03 +0200

    auth: fix handling of outdated session

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,25 +1,29 @@
 import React from "react";
-import { NavBar, Scroll, TopBar, SessionProvider } from "comps";
+import { NavBar, Scroll, TopBar, SessionProvider, Auth } from "comps";
 import { useStyles } from "hooks";
-import { useAuth } from "@nhost/react-auth";
 import { useRouter } from "next/router";
 
 export default function Layout({ children }: { children?: any }) {
   const { pathname } = useRouter();
-  const { signedIn } = useAuth();
   const classes = useStyles();
 
   if (["/screen", "/login"].includes(pathname))
-    return <SessionProvider>{signedIn != null && children}</SessionProvider>;
+    return (
+      <SessionProvider>
+        <Auth>{children}</Auth>
+      </SessionProvider>
+    );
 
   return (
     <SessionProvider>
-      <TopBar />
-      <Scroll>
-        {signedIn != null && children}
-        <div className={classes.pad} />
-      </Scroll>
-      <NavBar />
+      <Auth>
+        <TopBar />
+        <Scroll>
+          {children}
+          <div className={classes.pad} />
+        </Scroll>
+        <NavBar />
+      </Auth>
     </SessionProvider>
   );
 }
