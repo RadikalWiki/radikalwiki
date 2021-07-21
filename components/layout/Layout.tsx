c78c9ad232123e589d21bc5321b3e8e8c59f0a87
Niclas Overby <niclas@overby.me>
2021-07-21 11:05:30 +0200

    layout: add container

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -3,6 +3,7 @@ import { NavBar, Scroll, TopBar, SessionProvider } from "comps";
 import { useStyles } from "hooks";
 import { useAuth } from "@nhost/react-auth";
 import { useRouter } from "next/router";
+import { Container } from "@material-ui/core";
 
 export default function Layout({ children }: { children?: any }) {
   const { pathname } = useRouter();
@@ -15,10 +16,12 @@ export default function Layout({ children }: { children?: any }) {
   return (
     <SessionProvider>
       <TopBar />
-      <Scroll>
-        {signedIn != null && children}
-        <div className={classes.pad} />
-      </Scroll>
+      <Container>
+        <Scroll>
+          {signedIn != null && children}
+          <div className={classes.pad} />
+        </Scroll>
+      </Container>
       <NavBar />
     </SessionProvider>
   );
