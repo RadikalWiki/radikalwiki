233a4a34d8fab8dd3a7f277e48f53e354e2372d1
Niclas Overby <niclas@overby.me>
2021-07-26 09:00:01 +0200

    error: display error in production

---
+++ pages/_error.tsx
@@ -0,0 +1,19 @@
+function Error({ name, message, stack }: any) {
+  return (
+    <>
+      <p>An error occurred on client</p>
+      <p>{`Name: ${name}`}</p>
+      <p>{`Message: ${message}`}</p>
+      <p>Stack:</p>
+      {stack.split("\n").map((line: any) => (
+        <p>{line}</p>
+      ))}
+    </>
+  );
+}
+
+Error.getInitialProps = ({ res, err }: any) => {
+  return { name: err?.name, message: err?.message, stack: err?.stack };
+};
+
+export default Error;
