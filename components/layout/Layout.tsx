8cd820233fce864c9a0e86ceec26ead8ad091fec
Niclas Overby <niclas@overby.me>
2021-07-31 08:58:28 +0200

    Revert "auth: fix handling of outdated session"
    This reverts commit 91a2df25dc0225282f77d276b837c01c0f567b8b.

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,32 +1,28 @@
 import React from "react";
-import { NavBar, Scroll, TopBar, SessionProvider, Auth } from "comps";
+import { NavBar, Scroll, TopBar, SessionProvider } from "comps";
 import { useStyles } from "hooks";
+import { useAuth } from "@nhost/react-auth";
 import { useRouter } from "next/router";
 import { Container } from "@material-ui/core";
 
 export default function Layout({ children }: { children?: any }) {
   const { pathname } = useRouter();
+  const { signedIn } = useAuth();
   const classes = useStyles();
 
   if (["/screen", "/login"].includes(pathname))
-    return (
-      <SessionProvider>
-        <Auth>{children}</Auth>
-      </SessionProvider>
-    );
+    return <SessionProvider>{signedIn != null && children}</SessionProvider>;
 
   return (
     <SessionProvider>
-      <Auth>
-        <TopBar />
-        <Scroll>
-          <Container>
-            {children}
-            <div className={classes.pad} />
-          </Container>
-        </Scroll>
-        <NavBar />
-      </Auth>
+      <TopBar />
+      <Scroll>
+        <Container>
+          {signedIn != null && children}
+          <div className={classes.pad} />
+        </Container>
+      </Scroll>
+      <NavBar />
     </SessionProvider>
   );
 }
