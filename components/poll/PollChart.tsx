fb83ef67d581999f6b1c0f12bb1ff3c4bd4f4cf0
Niclas Overby <niclas@overby.me>
2021-12-12 20:09:27 +0100

    pollchart: simplify logic

--- components/poll/PollChart.tsx
+++ components/poll/PollChart.tsx
@@ -16,27 +16,26 @@ import {
 import { Card, CardHeader, Typography, Box } from "@mui/material";
 import { useSession } from "hooks";
 import { polls, Maybe, useSubscription } from "gql";
-import { Poll } from "@mui/icons-material";
 
-const parseData = (poll: any, screen: boolean, admin: boolean) => {
+const parseData = (poll: Maybe<polls>, screen: boolean, admin: boolean) => {
   if (!poll) {
     return { options: undefined, data: undefined };
   }
   if (poll.active || (poll.hidden && (screen || !admin))) {
     return {
       options: ["Antal Stemmer"],
-      data: [{ arg: "none", 0: [poll.count] }],
+      data: [{ arg: "none", 0: [poll.votes_aggregate().aggregate?.count()] }],
     };
   }
   let res: Record<string, any> = { arg: "none" };
   for (let i = 0; i < poll.options?.length; i++) {
     res[i] = 0;
   }
-  for (const vote of poll.votes) {
-    vote.value?.forEach((index: number) => {
+  poll.votes().map(({ value }) => {
+    value?.map((index: number) => {
       res[index] += 1;
     });
-  }
+  })
 
   return { options: poll.options, data: [res] };
 };
@@ -51,29 +50,18 @@ export default function PollChart({
   const [session, setSession] = useSession();
   const admin = session?.roles?.includes("admin") ?? false;
   const subscription = useSubscription();
-  const pollSub = subscription.polls_by_pk({ id: pollId });
+  const poll = subscription.polls_by_pk({ id: pollId });
   const folder = subscription.polls_by_pk({ id: pollId })?.content?.folder;
-  const poll = pollSub
-    ? {
-        active: pollSub.active,
-        hidden: pollSub.hidden,
-        options: pollSub.options,
-        count: pollSub.votes_aggregate().aggregate?.count(),
-        votes: pollSub.votes().map(({ value }) => ({ value })),
-      }
-    : null;
-  const voteCount = pollSub?.content?.folder?.event
+  const voteCount = poll?.content?.folder?.event
     ?.admissions_aggregate()
     .aggregate?.count();
 
   useEffect(() => {
-    if (folder) setSession({ path: folder.parentId ? [{ name: folder.name ?? "", url: `/folder/${folder.id}` }, { name: pollSub?.content?.name ?? "", url: `/content/${pollSub?.content?.id}` }, { name: "Afstemning", url: `/poll/${pollId}`, icon: "poll" }] : [] });
+    if (folder) setSession({ path: folder.parentId ? [{ name: folder.name ?? "", url: `/folder/${folder.id}` }, { name: poll?.content?.name ?? "", url: `/content/${poll?.content?.id}` }, { name: "Afstemning", url: `/poll/${pollId}`, icon: "poll" }] : [] });
   }, [folder]);
 
   if (poll == null) return null;
   const chartData = parseData(poll, screen, admin) || [];
-
-
 
   return (
     <Card elevation={3} sx={{ m: 1 }}>
@@ -82,7 +70,7 @@ export default function PollChart({
           bgcolor: (theme) => theme.palette.secondary.main,
           color: (theme) => theme.palette.secondary.contrastText,
         }}
-        title={pollSub?.content?.name}
+        title={poll?.content?.name}
       />
       {chartData?.options?.map((opt: any, index: number) => (
         <Box
