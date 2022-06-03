041ee2918b9100c3b2dd750736ae4ddea3c2fa34
Niclas Overby <niclas@overby.me>
2022-06-04 00:55:38 +0200

    nhost: use cookies

--- core/nhost.tsx
+++ core/nhost.tsx
@@ -3,6 +3,7 @@ import { NhostClient } from "@nhost/react";
 const { auth, functions, graphql, storage } = new NhostClient({
   autoRefreshToken: false,
   backendUrl: process.env.NEXT_PUBLIC_NHOST_BACKEND ?? "",
+  clientStorageType: "cookie",  
 });
 
 export { auth, functions, graphql, storage };
