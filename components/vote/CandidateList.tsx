ce736819e88652d253559748fd20d94e52b5c6c8
Niclas Overby <niclas@overby.me>
2022-07-03 22:08:19 +0200

    CandidateList: only show immutable candidates

--- components/vote/CandidateList.tsx
+++ components/vote/CandidateList.tsx
@@ -14,11 +14,13 @@ import { useRouter } from "next/router";
 import { query, resolved } from "gql";
 import { toWhere } from "core/path";
 import { Box } from "@mui/system";
+import { useUserId } from "@nhost/react";
 
 export default function CandidateList() {
   const node = useNode();
   const router = useRouter();
   const largeScreen = useMediaQuery("(min-width:640px)");
+  const userId = useUserId();
 
   const handleOnClick = (namespace?: string) => async () => {
     const path = `${router.asPath}/${namespace}`.substring(1).split("/");
@@ -34,7 +36,20 @@ export default function CandidateList() {
   return (
     <ImageList cols={largeScreen ? 2 : 1} sx={{ m: 1 }}>
       {node.query
-        ?.children({ where: { mimeId: { _eq: "vote/candidate" } } })
+        ?.children({
+          where: {
+            _and: [
+              { mimeId: { _eq: "vote/candidate" } },
+              {
+                _or: [
+                  { mutable: { _eq: false } },
+                  { ownerId: { _eq: userId } },
+                  { members: { nodeId: { _eq: userId } } },
+                ],
+              },
+            ],
+          },
+        })
         .map((child) => {
           const image = child.data()?.image;
           return (
@@ -46,8 +61,7 @@ export default function CandidateList() {
               {image ? (
                 <Image alt="Billede for indhold" layout="fill" src={image} />
               ) : (
-                <Box sx={{ height: "50px" }}>
-                </Box>
+                <Box sx={{ height: "50px" }}></Box>
               )}
               <ImageListItemBar
                 sx={{ borderRadius: "10px" }}
