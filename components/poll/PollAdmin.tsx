2a2eda55be896aaac4d2da11cd1257b99bd651e0
Niclas Overby <niclas@overby.me>
2022-07-31 08:44:58 +0200

    PollAdmin: store voters

--- components/poll/PollAdmin.tsx
+++ components/poll/PollAdmin.tsx
@@ -8,10 +8,31 @@ import { Stop } from "@mui/icons-material";
 export default function PollAdmin({ node }: { node: Node }) {
   const data = node.sub?.data();
   const refetch = useRefetch();
- 
+
+  const voters = node.sub?.context
+    ?.permissions({
+      where: {
+        _and: [
+          { mimeId: { _eq: "vote/vote" } },
+          { insert: { _eq: true } },
+          {
+            node: {
+              members: { active: { _eq: true } },
+            },
+          },
+        ],
+      },
+    })
+    .map((perm) => perm.node?.members_aggregate().aggregate?.count())
+    .reduce((total, next) => (total ?? 0) + (next ?? 0), 0);
+
   const handleStopPoll = async (_: any) => {
-    await node.update({ set: { mutable: false, data: { ...data } } });
-    await refetch(() => node.query?.children({ where: { mimeId: { _eq: "vote/vote" } } }).map(vote => vote.data))
+    await node.update({ set: { mutable: false, data: { ...data, voters } } });
+    await refetch(() =>
+      node.query
+        ?.children({ where: { mimeId: { _eq: "vote/vote" } } })
+        .map((vote) => vote.data)
+    );
   };
 
   if (!node.sub?.mutable || !node.sub?.isContextOwner) return null;
