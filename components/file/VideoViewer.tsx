9d329c72b54577b6e61402fd9e513abf86ffab14
Niclas Overby <niclas@overby.me>
2022-08-04 22:46:35 +0200

    VideoViewer: autoplay

--- components/file/VideoViewer.tsx
+++ components/file/VideoViewer.tsx
@@ -1,7 +1,7 @@
 export default function VideoViewer({ file }: { file: any }) {
   return (
     file && (
-      <video width="100%" controls>
+      <video autoPlay width="100%" controls>
         <source src={file} type="video/mp4" />
       </video>
     )
