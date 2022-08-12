b4715da6578fe0703464e846d246554bce32fe72
Niclas Overby <niclas@overby.me>
2022-08-12 13:09:33 +0200

    QuestionList: fix performance issue

--- components/vote/QuestionList.tsx
+++ components/vote/QuestionList.tsx
@@ -38,74 +38,14 @@ import { nodes, order_by } from "gql";
 import { getIconFromId } from "mime";
 import { Node, useScreen } from "hooks";
 import { TransitionGroup } from "react-transition-group";
-
-function ChildListElement({
-  node,
-  child,
-  index,
-}: {
-  node: Node;
-  child?: nodes;
-  index: number;
-}) {
-  const $delete = node.useDelete();
-  const router = useRouter();
-  const id = child?.id;
-
-  const item = (
-    <>
-      <ListItem
-        button
-        component={NextLink}
-        href={`${router.asPath}/${child?.namespace}`}
-      >
-        <ListItemAvatar>
-          <Avatar
-            sx={{
-              bgcolor: (t) => t.palette.secondary.main,
-            }}
-          >
-            {index + 1}
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary={child?.data()?.text}
-          secondary={
-            <Chip
-              key={child?.owner?.id}
-              icon={<Face />}
-              color="secondary"
-              variant="outlined"
-              size="small"
-              sx={{ mr: 0.5 }}
-              label={child?.owner?.displayName}
-            />
-          }
-        />
-        {(child?.isOwner || child?.isContextOwner) && (
-          <ListItemSecondaryAction>
-            <IconButton
-              color="primary"
-              onClick={() => {
-                $delete({ id });
-              }}
-              size="large"
-            >
-              <Delete />
-            </IconButton>
-          </ListItemSecondaryAction>
-        )}
-      </ListItem>
-    </>
-  );
-
-  return child?.id ? item : null;
-}
+import { useUserId } from "@nhost/react";
 
 export default function QuestionList({ node }: { node: Node }) {
   const screen = useScreen();
   const router = useRouter();
   const query = node.useQuery();
+  const $delete = node.useDelete();
+  const userId = useUserId();
   const children = query?.children({
     where: { mimeId: { _eq: "vote/question" } },
     order_by: [{ index: order_by.asc }],
@@ -125,29 +65,70 @@ export default function QuestionList({ node }: { node: Node }) {
           </Avatar>
         }
         action={
-          !screen && <CardActions sx={{ p: 0 }}>
-            {query?.isContextOwner && !screen && (
-              <AutoButton
-                text="Sorter"
-                icon={<LowPriority />}
-                onClick={() => router.push(`${router.asPath}?app=sort`)}
-              />
-            )}
-            <AddQuestionButton node={node} />
-          </CardActions>
+          !screen && (
+            <CardActions sx={{ p: 0 }}>
+              {query?.isContextOwner && !screen && (
+                <AutoButton
+                  text="Sorter"
+                  icon={<LowPriority />}
+                  onClick={() => router.push(`${router.asPath}?app=sort`)}
+                />
+              )}
+              <AddQuestionButton node={node} />
+            </CardActions>
+          )
         }
       />
       <Divider />
       <List>
         <TransitionGroup>
           {children?.map((child, index) => {
-            return (
+            const item = (
               <Collapse key={child?.id ?? 0}>
-                <Suspense fallback={null}>
-                  <ChildListElement node={node} child={child} index={index} />
-                </Suspense>
+                  <ListItem
+                    button
+                    component={NextLink}
+                    href={`${router.asPath}/${child?.namespace}`}
+                  >
+                    <ListItemAvatar>
+                      <Avatar
+                        sx={{
+                          bgcolor: (t) => t.palette.secondary.main,
+                        }}
+                      >
+                        {index + 1}
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={child?.data()?.text}
+                      secondary={
+                        <Chip
+                          key={child?.owner?.id}
+                          icon={<Face />}
+                          color="secondary"
+                          variant="outlined"
+                          size="small"
+                          sx={{ mr: 0.5 }}
+                          label={child?.owner?.displayName}
+                        />
+                      }
+                    />
+                    {((child.ownerId == userId || query?.isContextOwner) && <ListItemSecondaryAction>
+                          <IconButton
+                            color="primary"
+                            onClick={() => {
+                              $delete({ id: child.id });
+                            }}
+                            size="large"
+                          >
+                            <Delete />
+                          </IconButton>
+                        </ListItemSecondaryAction>
+                      )}
+                  </ListItem>
               </Collapse>
             );
+            return child?.id ? item : null;
           })}
           {!children?.[0]?.id && (
             <Collapse key={-1}>
