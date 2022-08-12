6bad91852365fd0f4b1934375f7668d66d7f2724
Niclas Overby <niclas@overby.me>
2022-08-12 23:45:11 +0200

    OldBrowser: ignore Opera

--- core/util.ts
+++ core/util.ts
@@ -6,7 +6,7 @@ const checkVersion = () => {
     case "Gecko":
       if (compare(platform.version ?? "0", "94", "<=")) return false;
     case "Blink":
-      if (compare(platform.version ?? "0", "98", "<=")) return false;
+      if (compare(platform.version ?? "0", "98", "<=") && platform.name !== "Opera") return false;
     case "WebKit":
       if (compare(platform.version ?? "0", "15.4", "<=")) return false;
     default:
