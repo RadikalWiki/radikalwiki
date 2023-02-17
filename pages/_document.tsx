78bfd8bea29e97465596e6249e34c549c444d0c8
Niclas Overby <niclas@overby.me>
2023-02-17 21:17:52 +0100

    document: set danish as default lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="da">
       <Head>
         <link
           rel="stylesheet"
