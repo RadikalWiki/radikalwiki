aa5e9d6c68454b00a2d5920a443572fc43f44dfc
Niclas Overby <niclas@overby.me>
2023-03-07 18:30:49 +0100

    MemberChips: handle overflow

--- components/content/MemberChips.tsx
+++ components/content/MemberChips.tsx
@@ -1,5 +1,12 @@
 import { Face } from '@mui/icons-material';
-import { Chip, Collapse, Skeleton, Tooltip, Typography } from '@mui/material';
+import {
+  Chip,
+  Collapse,
+  Grid,
+  Skeleton,
+  Tooltip,
+  Typography,
+} from '@mui/material';
 import { Stack } from '@mui/system';
 import { Node, useScreen } from 'hooks';
 import { IconId } from 'mime';
@@ -9,24 +16,26 @@ const MemberChips = ({ node, child }: { node: Node; child?: boolean }) => {
   const members = node.useQuery()?.members();
   const chips =
     members?.map(({ id, name, node, user }) => (
-      <Tooltip key={id ?? 0} title="Forfatter">
-        <Chip
-          icon={node?.mimeId ? <IconId mimeId={node?.mimeId} /> : <Face />}
-          size={screen ? 'medium' : 'small'}
-          color="secondary"
-          label={
-            <Typography variant={screen ? 'h5' : undefined}>
-              {name ?? user?.displayName}
-            </Typography>
-          }
-        />
-      </Tooltip>
+      <Grid item key={id ?? 0}>
+        <Tooltip  title="Forfatter">
+          <Chip
+            icon={node?.mimeId ? <IconId mimeId={node?.mimeId} /> : <Face />}
+            size={screen ? 'medium' : 'small'}
+            color="secondary"
+            label={
+              <Typography variant={screen ? 'h5' : undefined}>
+                {name ?? user?.displayName}
+              </Typography>
+            }
+          />
+        </Tooltip>
+      </Grid>
     )) ?? [];
   return (
     <Collapse in={!!members?.[0]?.id && members?.length !== 0}>
-      <Stack direction="row" spacing={0.5} sx={{ p: child ? 0 : 1 }}>
+      <Grid container spacing={0.5} sx={{ p: child ? 0 : 1 }}>
         {chips}
-      </Stack>
+      </Grid>
     </Collapse>
   );
 };
