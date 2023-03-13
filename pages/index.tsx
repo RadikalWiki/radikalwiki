57405a1b26d454f879055fe25e92770fb8b5c2d0
Niclas Overby <niclas@overby.me>
2023-03-13 15:30:31 +0100

    index: format using prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,15 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 const Index = () => {
-	const router = useRouter()
+  const router = useRouter();
 
-	useEffect(() => {
-		if (router.query.type === "passwordReset") {
-			router.push("/user/set-password")
-		}
-	}, [router.query])
+  useEffect(() => {
+    if (router.query.type === 'passwordReset') {
+      router.push('/user/set-password');
+    }
+  }, [router.query]);
 
-	return <Loader app="home" />;
-}
+  return <Loader app="home" />;
+};
 
 export default Index;
