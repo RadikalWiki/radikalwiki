3f5e58233aae5d9f8ece5ac158e9d67803fad46a
Niclas Overby <niclas@overby.me>
2023-03-13 15:29:36 +0100

    nhost: redirect to set password with recovery link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
 import { Loader } from 'comps';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-const Index = () => <Loader app="home" />;
+const Index = () => {
+	const router = useRouter()
+
+	useEffect(() => {
+		if (router.query.type === "passwordReset") {
+			router.push("/user/set-password")
+		}
+	}, [router.query])
+
+	return <Loader app="home" />;
+}
 
 export default Index;
