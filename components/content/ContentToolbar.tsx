a3e19842a4d0013e82529f286564e859e3ea8c3c
Niclas Overby <niclas@overby.me>
2023-05-21 22:34:14 +0200

    ContentToolbar: fix add content in folder

--- components/content/ContentToolbar.tsx
+++ components/content/ContentToolbar.tsx
@@ -9,28 +9,36 @@ import {
   PublishButton,
 } from 'comps';
 
-const ContentToolbar = ({ node, child, add }: { node: Node; child?: boolean, add?: boolean }) => {
+const ContentToolbar = ({
+  node,
+  child,
+  add,
+}: {
+  node: Node;
+  child?: boolean;
+  add?: boolean;
+}) => {
   const query = node.useQuery();
   const link = useLink();
 
-  if (child) return null;
-
   return (
     <Stack spacing={1} direction="row">
-      <DownloadButton node={node} />
-      <ButtonGroup>
-        {((query?.mutable && query?.isOwner) || query?.isContextOwner) && [
-          <DeleteButton key="delete" node={node} />,
-          <AutoButton
-            key="edit"
-            text="Rediger"
-            icon={<Edit />}
-            onClick={() => link.push([], 'editor')}
-          />,
-          <PublishButton key="publish" node={node} />
-        ]}
-      </ButtonGroup>
-      {add &&<AddContentButton node={node} />}
+      {!child && <DownloadButton node={node} />}
+      {!child && (
+        <ButtonGroup>
+          {((query?.mutable && query?.isOwner) || query?.isContextOwner) && [
+            <DeleteButton key="delete" node={node} />,
+            <AutoButton
+              key="edit"
+              text="Rediger"
+              icon={<Edit />}
+              onClick={() => link.push([], 'editor')}
+            />,
+            <PublishButton key="publish" node={node} />,
+          ]}
+        </ButtonGroup>
+      )}
+      {add && <AddContentButton node={node} />}
     </Stack>
   );
 };
