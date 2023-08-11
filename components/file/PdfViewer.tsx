5c87e5d0d0634067a6b4e4591892ce15e0a25834
Niclas Overby <niclas@overby.me>
2023-08-11 21:36:55 +0200

    PdfReader: fix Safari support

--- components/file/PdfViewer.tsx
+++ components/file/PdfViewer.tsx
@@ -1,6 +1,6 @@
 const PdfViewer = ({ file }: { file?: string }) => (
   <div style={{ width: '100%', height: '75vh' }}>
-    <object data={file} type="application/pdf" width="100%" height="100%" />
+    <iframe src={file} width="100%" height="100%" />
   </div>
 );
 
