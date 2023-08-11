82cc7725a25fb457af1dcf0d421726cb3cc5b2d5
Niclas Overby <niclas@overby.me>
2023-08-11 21:45:32 +0200

    PdfReader: remove border

--- components/file/PdfViewer.tsx
+++ components/file/PdfViewer.tsx
@@ -1,6 +1,6 @@
 const PdfViewer = ({ file }: { file?: string }) => (
   <div style={{ width: '100%', height: '75vh' }}>
-    <iframe src={file} width="100%" height="100%" />
+    <iframe frameBorder="0" src={file} width="100%" height="100%" />
   </div>
 );
 
