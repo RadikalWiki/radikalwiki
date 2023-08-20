1c763bbc7126362e2452c24058f6796c1a57c2df
Niclas Overby <niclas@overby.me>
2023-08-20 23:09:16 +0200

    pkgs: bump gqty

--- core/gql/index.ts
+++ core/gql/index.ts
@@ -50,7 +50,7 @@ const queryFetcher: QueryFetcher = async (
 const cache = new Cache(undefined, {
   maxAge: Infinity,
   staleWhileRevalidate: 5 * 60 * 1000,
-  normalization: true,
+  normalization: false,
 });
 
 const subscriptionsClient = createSubscriptionsClient({
