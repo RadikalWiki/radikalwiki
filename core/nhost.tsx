c0a027d355eb4f3307692c7f83ce43448d033ad6
Niclas Overby <niclas@overby.me>
2023-12-08 16:02:22 +0100

    nhost: set region

--- core/nhost.tsx
+++ core/nhost.tsx
@@ -2,6 +2,7 @@ import { NhostClient } from '@nhost/nextjs';
 
 const nhost = new NhostClient({
   backendUrl: process.env.NEXT_PUBLIC_NHOST_BACKEND,
+  region: "eu-central-1"
 });
 
 export { nhost };
