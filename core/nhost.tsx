c08e4238dbf04084e5dbb65f33daa42efd83fc40
Niclas Overby <niclas@overby.me>
2023-12-08 16:19:35 +0100

    nhost: fix subdomain

--- core/nhost.tsx
+++ core/nhost.tsx
@@ -1,7 +1,7 @@
 import { NhostClient } from '@nhost/nextjs';
 
 const nhost = new NhostClient({
-  backendUrl: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN,
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN,
   region: process.env.NEXT_PUBLIC_NHOST_REGION
 })
 
