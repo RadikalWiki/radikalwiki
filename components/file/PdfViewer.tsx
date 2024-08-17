02f0d63bc4c6d8efa7e11dad61463c17dd2eab17
Niclas Overby <niclas@overby.me>
2024-08-17 12:00:09 +0200

    PdfViewer: use Google Docs

--- components/file/PdfViewer.tsx
+++ components/file/PdfViewer.tsx
@@ -1,7 +1,28 @@
-const PdfViewer = ({ file }: { file?: string }) => (
-  <div style={{ width: '100%', height: '75vh' }}>
-    <iframe frameBorder="0" src={file} width="100%" height="100%" />
-  </div>
-);
+import { startTransition, useEffect, useState } from 'react';
+import { useScreen } from 'hooks';
+
+const PdfViewer = ({ file }: { file?: string }) => {
+  const screen = useScreen();
+  const [height, setHeight] = useState('0px');
+
+  useEffect(() => {
+    const scroll =
+      document.querySelector('#scroll') ?? document.scrollingElement;
+    startTransition(() => {
+      setHeight(`${(scroll?.scrollHeight ?? 0) - (screen ? 100 : 210)}px`);
+    });
+  }, []);
+
+  return file ? (
+    <iframe
+      width="100%"
+      height={height}
+      frameBorder="0"
+      src={`https://docs.google.com/viewer?url=${encodeURIComponent(
+        file
+      )}&embedded=true`}
+    />
+  ) : null;
+};
 
 export default PdfViewer;
